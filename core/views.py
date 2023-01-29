f48bce91142d592981052f9fe54e21681d2172eb
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-29 07:53:54 +0100

    show products on frontpage

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from products.models import Category, Product
+
 # Create your views here.
 
 
 def frontpage(request):
-    return render(request, "core/frontpage.html")
+    products = Product.objects.all()[0:8]
+    return render(request, "core/frontpage.html", {"products": products})
