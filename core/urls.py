666879dab7f93407894211d878d47b8e449f73a1
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-29 08:29:33 +0100

    product detail

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,6 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.frontpage, name="frontpage"),
+    path("shop/", views.shop, name="shop"),
+    path("products/<slug:slug>/", views.products_detail, name="products_detail"),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,18 @@ from products.models import Category, Product
 def frontpage(request):
     products = Product.objects.all()[0:8]
     return render(request, "core/frontpage.html", {"products": products})
+
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, "core/shop.html", {"products": products})
+
+
+def get_categories(request):
+    categories = Category.objects.all()
+    return {"all_categories": categories}
+
+
+def products_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, "core/products_detail.html", {"product": product})
